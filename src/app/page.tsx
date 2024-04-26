905c3cf3ff60acf08729a7ae4359619d4a3b239c
fbciriaco <fb.ciriaco@gmail.com>
2024-04-26 13:00:53 -0300

    Fix dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
-import HeadlineFlex from "@/components/Headline/HeadlineFlex";
-import HeadlineGrid from "@/components/Headline/HeadlineGrid";
+import HeadlineFlex from "@/app/components/Headline/HeadlineFlex";
+import HeadlineGrid from "@/app/components/Headline/HeadlineGrid";
 
 export default function Home() {
   return (
-    <main className="flex flex-col">
-      <h1>Noticias Inventadas</h1>
-    </main>
-    <div style={{border: 1 }}>
-      <HeadlineFlex
-      title="Este é um título teste para uma notícia"
-      category="Teste"
-      imgUrl="https://picsum.photos/600/400"
-      /> 
-      <HeadlineGrid
-      title="Este é um título teste para outra notícia (usando grid)"
-      category="Teste"
-      imgUrl="https://picsum.photos/600/400?grayscale"
-      /> 
+    <div>
+      <main className="flex flex-col">
+        <h1>Noticias Inventadas</h1>
+      </main>
+      <div style={{ border: 1 }}>
+        <HeadlineFlex
+          title="Este é um título teste para uma notícia"
+          category="Teste"
+          imgUrl="https://picsum.photos/600/400"
+        />
+        <HeadlineGrid
+          title="Este é um título teste para outra notícia (usando grid)"
+          category="Teste"
+          imgUrl="https://picsum.photos/600/400?grayscale"
+        />
+      </div>
     </div>
-  )
+  );
 }
