db51fd28caf59692c3c99e102b32c5c893508ec3
Jennifer Bird <jennifer.bird@awaze.com>
2024-05-25 18:25:19 +0100

    Added burger menu

--- src/app/_components/Nav/Nav.tsx
+++ src/app/_components/Nav/Nav.tsx
@@ -4,16 +4,18 @@ import SectionContainer from "../Layout/SectionContainer";
 import { Logo } from "../Logo/Logo";
 import { SearchInput } from "../SearchInput/SeachInput";
 import { NavLinksContainer } from "./NavLinksContainer";
+import { isMobileDeviceSSR } from "@/app/_utils/serverResponsive";
 
 const Nav: React.FC = () => {
+  const isMobileSSR = isMobileDeviceSSR();
   return (
     <nav className="h-[80px] bg-main flex w-full shadow-md">
       <SectionContainer customClasses="flex justify-between items-center py-[10px]">
         <Logo />
-        <div className="flex-1 mx-8">
+        <div className="flex-1 ml-0 mr-12 md:mx-8">
           <SearchInput />
         </div>
-        <NavLinksContainer />
+        <NavLinksContainer isMobileSSR={isMobileSSR} />
       </SectionContainer>
     </nav>
   );
--- src/app/_components/Nav/NavLinksContainer.tsx
+++ src/app/_components/Nav/NavLinksContainer.tsx
@@ -3,16 +3,18 @@
 import { useClientMediaQuery } from "@/app/_hooks/useClientMediaQuery";
 import { MOCK_LINKS } from "@/app/_mock/mock";
 import { CustomLink } from "../Link/Link";
+import { BurgerNav } from "./BurgerNav";
 
-export const NavLinksContainer = () => {
+export const NavLinksContainer = ({ isMobileSSR = false }) => {
   const isSmallerScreen = useClientMediaQuery("(max-width: 1024px)");
-  return !isSmallerScreen ? (
+
+  return isMobileSSR || isSmallerScreen ? (
+    <BurgerNav links={MOCK_LINKS} />
+  ) : (
     <div className="flex gap-4">
       {MOCK_LINKS.map(({ path, text }) => (
         <CustomLink text={text} path={path} key={path} />
       ))}
     </div>
-  ) : (
-    <div>nav</div>
   );
 };
